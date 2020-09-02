1659e17c492a77f182cd194f1c677a0fed1aca53
Luis Chiaradia <luisfelipe.sbs@gmail.com>
2020-09-02 15:41:48 -0300

    adicionando readme file

---
+++ README.md
@@ -0,0 +1,26 @@
+# AME QA Web
+Repositório do AME QA Challenge - Web Application
+
+## Como executar o projeto
+
+* Importante ter o Ruby instalado (versão 2.5 ou superior)
+
+### Instalar o Bundler
+`
+gem install bundler
+`
+
+### Instalar as dependencias do ruby (projeto)
+`
+bundle install
+`
+
+### Executar localmente (minha maquina)
+`
+bundle exec cucumber
+`
+
+### Executar gerando report .JSON para construção do report HTML
+`
+bundle exec cucumber -p json
+`
--- features/hooks/hooks.rb
+++ features/hooks/hooks.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+require 'report_builder'
+require 'date'
 
 Before do
   @new_user = NewUserPage.new
@@ -6,4 +8,20 @@ Before do
 
   @file = YAML.load_file(File.join(Dir.pwd, 'features/support/fixtures/dados.yaml'))
   page.current_window.resize_to(1920, 1080)
+end
+
+today_date = DateTime.now
+@current_date = today_date.to_s.tr(':', '-')
+
+at_exit do
+  ReportBuilder.configure do |config|
+    config.input_path = 'log/report.json'
+    config.report_path = "log/report_#{@current_date}"
+    config.report_types = [:html]
+    config.report_title = 'QA Challenge - WebApp'
+    config.compress_image = true
+    config.additional_info = { 'App': 'Web', 'Execution Date': @current_date }
+    config.color = 'indigo'
+  end
+  ReportBuilder.build_report
 end
